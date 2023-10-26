1ed7a68b2ffdd140ccd892b7d09305e64a82951e
Ranojaan Mahendran <50835745+Ranojaan@users.noreply.github.com>
2023-10-26 10:24:06 +0530

    Add files via upload

---
+++ client/src/Post.js
@@ -0,0 +1,54 @@
+export default function Post(){
+    return (
+        <div className="post">
+        <div className="image">
+         <img src="https://forum.affinity.serif.com/uploads/monthly_2022_09/albert-camus.webp.98a85b0a8c5e7c0a87fea7ed26b222c9.webp"  alt=""></img>
+        </div>
+          <div className="text">
+            <h2>Albert Camus</h2>
+            <p className="info">
+              <a className="author">Ranojaan</a>
+              <time>2023-10-19 16:44</time>
+            </p>
+            <p className="summary">Albert Camus was a French-Algerian philosopher, author, and playwright known for his influential works on existentialism and absurdism. His most famous novel, "The Stranger," explores the idea of the absurdity of life and the human condition. Camus' philosophy often grappled with the meaning of life in a world devoid of inherent purpose, and he emphasized the importance of embracing life's absurdity while finding personal meaning and happiness.</p>
+          </div>  
+
+          <div className="image">
+         <img src="https://m.media-amazon.com/images/M/MV5BYjA3M2Q0ZjUtMDM5My00OGQ5LWI0NGQtMTI3ZWU0M2NiM2NkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTc4MzI2NQ@@._V1_.jpg"  alt=""></img>
+        </div>
+          <div className="text">
+            <h2>Jean-Paul Sartre</h2>
+            <p className="info">
+              <a className="author">Arun</a>
+              <time>2023-07-24 08:28</time>
+            </p>
+            <p className="summary">Jean-Paul Sartre, a French existentialist philosopher and writer, is best known for his work "Being and Nothingness," which explores the concept of bad faith and the idea that individuals are responsible for creating their own meaning in life. His plays, like "No Exit," reflect existentialist themes and the absurdity of human existence, leaving a lasting impact on both philosophy and literature.</p>
+          </div>  
+
+           <div className="image">
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Nietzsche187a.jpg/330px-Nietzsche187a.jpg"  alt=""></img>
+        </div>
+          <div className="text">
+            <h2>Friedrich Nietzsche</h2>
+            <p className="info">
+              <a className="author">Icarus</a>
+              <time>2023-11-29 12:32</time>
+            </p>
+            <p className="summary">Friedrich Nietzsche, a German philosopher, is renowned for his critical and provocative ideas. He challenged traditional morality, championing the concept of the "Übermensch" or "overman," emphasizing individual will and power. His works, such as "Thus Spoke Zarathustra" and "Beyond Good and Evil," continue to influence contemporary philosophy and various fields of thought.</p>
+          </div> 
+
+           <div className="image">
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Noam_Chomsky_portrait_2017_retouched.png/330px-Noam_Chomsky_portrait_2017_retouched.png"  alt=""></img>
+        </div>
+          <div className="text">
+            <h2>Noam Chomsky</h2>
+            <p className="info">
+              <a className="author">Kamal</a>
+              <time>2023-01-19 18:13</time>
+            </p>
+            <p className="summary">Noam Chomsky is an American linguist, philosopher, cognitive scientist, and political commentator. He is renowned for his groundbreaking work in the field of linguistics, particularly his development of transformational-generative grammar. Chomsky's ideas about the innate structures of human language have had a profound impact on linguistics and cognitive science. Additionally, he is known for his critical analysis of media, politics, and the role of intellectual dissent in contemporary society.</p>
+          </div>  
+
+      </div>
+    );
+}
